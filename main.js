132985957c0140101a5b1ae1c294f220abd776cf
husiddiqui <35558454+husiddiqui@users.noreply.github.com>
2023-11-23 05:52:03 -0800

    UPDATE

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 let a ="home"
 console.log(a);
+//Primitive Data Type (Call by value)
+//7 type
+//strings,number,boolien,null,undefined,symbol,bigint
+//Reference (Non premitive)
+//Array,Objects, Functions
